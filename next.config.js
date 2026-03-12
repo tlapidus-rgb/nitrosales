d02ea30fdc3d6c41bb5ec1304481a346cd8a2bc3
tlapidus-rgb <tlapidus@99media.com.ar>
2026-03-12 14:39:47 -0300

    cleanup: revert next.config.js to clean state (build errors fixed)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+/** @type {import("next").NextConfig} */
+const nextConfig = {};
 
 module.exports = nextConfig;
